69e4e761829af376cf4881e954d3cab879855599
Carenalga <mapedorr@gmail.com>
2023-11-03 23:47:34 +0100

    Use inventory items (2/2)

--- popochiu/autoloads/a.gd
+++ popochiu/autoloads/a.gd
@@ -22,5 +22,7 @@ var sfx_door_close: AudioCueSound = load("res://popochiu/rooms/house/props/door/
 var sfx_door_open: AudioCueSound = load("res://popochiu/rooms/house/props/door/audio/sfx_door_open.tres")
 var sfx_container_locked: AudioCueSound = load("res://popochiu/rooms/outside/props/container/audio/sfx_container_locked.tres")
 var sfx_container_open: AudioCueSound = load("res://popochiu/rooms/outside/props/container/audio/sfx_container_open.tres")
+var sfx_apple_fall: AudioCueSound = load("res://popochiu/rooms/outside/props/tree/audio/sfx_apple_fall.tres")
+var sfx_tree_impact: AudioCueSound = load("res://popochiu/rooms/outside/props/tree/audio/sfx_tree_impact.tres")
 # ---- cues
 
