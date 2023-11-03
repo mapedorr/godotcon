36ef31ab99bed011f2b654ce36df6d1f9f922e9e
Carenalga <mapedorr@gmail.com>
2023-11-03 23:40:01 +0100

    Use inventory items (1/2)

--- popochiu/autoloads/i.gd
+++ popochiu/autoloads/i.gd
@@ -3,13 +3,16 @@ extends "res://addons/popochiu/engine/interfaces/i_inventory.gd"
 
 # classes ----
 const PIIKey := preload('res://popochiu/inventory_items/key/item_key.gd')
+const PIIHook := preload('res://popochiu/inventory_items/hook/item_hook.gd')
 # ---- classes
 
 # nodes ----
 var Key: PIIKey : get = get_Key
+var Hook: PIIHook : get = get_Hook
 # ---- nodes
 
 # functions ----
 func get_Key() -> PIIKey: return super.get_item_instance('Key')
+func get_Hook() -> PIIHook: return super.get_item_instance('Hook')
 # ---- functions
 
