9e818ce4e85b68c66f0ef4d71c6afe501a34f815
Carenalga <mapedorr@gmail.com>
2023-11-04 00:34:12 +0100

    Create a room with background music

---
+++ popochiu/autoloads/a.gd
@@ -0,0 +1,7 @@
+@tool
+extends "res://addons/popochiu/engine/interfaces/i_audio.gd"
+
+# cues ----
+var mx_house: AudioCueMusic = load("res://popochiu/rooms/house/audio/mx_house.tres")
+# ---- cues
+
